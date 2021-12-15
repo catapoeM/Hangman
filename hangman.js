5b614c772605202d7fa597c8967b802b7884abc2
Poenaru Catalin <69017468+catapoeM@users.noreply.github.com>
2021-12-15 19:23:38 +0100

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,49 +1,111 @@
-
 function play() {
-	alert("merge");
-	var words = ['car', 'banana', 'apple', 'dracula', 'motorcycle', 'country', 'bite', 'vampire'];
 
-	// We create the alphabetical buttons
-	var p, letter, button, holder;
-	holder = document.getElementById( "alphabet" );
-	// Create alphabet
-	for ( var i = 97; i <= 122; ++i ) {
-		if ( i == 97) {
-			p = document.createElement( "p" );
-		}
-		// Every number is transformed in letters
-		letter = String.fromCharCode( i );
-		// Every button is created
-		button = document.createElement( "button" );
-		// On every button is written a letter
-		button.innerHTML = letter;
-		button.setAttribute( "data-letter", letter );
-		button.onclick = function( sendLetter ) {
-		 	pickLetter( this.getAttribute( "data-letter" ) );
-		 	getButton( this.getAttribute(button));
-		};
-		p.appendChild( button );
-		if ( i == 106 ) {
-		    holder.appendChild( p );
-		    
+	var words = ['car', 'banana', 'apple', 'dracula', 'motorcycle',
+	 'country', 'bite', 'vampire'];
+
+	var underline = [];
+	var buttons = [];
+	var lives = 0;
+	var wordCheck = 0;
+	var info = 0;
+	createAlphabet();
+    createGuess();
+    
+    
+    function createAlphabet() {
+    	lives = 5;
+    	var letter;
+    	for (var i = 97; i <= 122; ++i) {
+	  		buttons[i] = document.createElement('button');
+	  		letter = String.fromCharCode(i);
+	  		buttons[i].innerHTML = letter;
+	  		buttons[i].setAttribute("data-letter", letter);
+	  		buttons[i].onclick = function(letter) {
+	  			pickLetter(this.getAttribute("data-letter"))
+	  		};
+  			document.getElementById('alphabet').appendChild(buttons[i]);
+  		}
+  		info = document.getElementById('info');
+		info.innerHTML = "GUESS THE WORD, YOU HAVE " + lives + " LIVES";
+  		
+    }
+  	
+  	function createGuess() {
+  		var letterUnder = 0;
+		var unknownWord = 0;
+		let choseRandom = Math.floor((Math.random() * 8));
+		wordCheck = words[choseRandom].length;
+		for (var i = 0, unknownWord = words[choseRandom]; i < 1; ++i) {
+			for (var j = 0; j < unknownWord.length; ++j) {
+				letterUnder = unknownWord[j];
+				underline[j] = document.createElement('button');
+				underline[j].innerHTML = "_";
+				underline[j].setAttribute("data-letter", letterUnder);
+				document.getElementById("unknownWord").appendChild(underline[j]);
+				
+			}
 		}
 	}
-	
-	function pickLetter(letter) {
-		alert(letter);
-		
-	}
 
-	underlineHolder = document.getElementById( "wordToGuess" );
-	var c, letterWord, buttonLetter; 
-	p = document.createElement( "p" );
-	let choseRandom = Math.floor((Math.random() * 9) + 1);
-	for (var i = 0, wordToGuess = words[choseRandom]; i < 1; ++i) {
-		for (var j = 0; j < wordToGuess.length; ++j) {
-			//alert(wordToGuess[j]);
-			//alert(wordToGuess[j].charCodeAt(0));
-		}
-	}
 
+ 	function pickLetter(letter) {
+ 		var idxLetter = letter.charCodeAt(letter);
+ 		buttons[idxLetter].setAttribute("disabled", "");
+ 		
+ 		var found = 0;
+		for (var i = 0; i < underline.length; ++i) {
+    	var guess = underline[i].getAttribute("data-letter");
+    		if (letter == guess) {
+    			found = 1;
+    			underline[i].setAttribute("data-letter", letter);
+    			underline[i].innerHTML = letter;
+    			--wordCheck;
+    		}
+    	}
+    	if (found == 0 && lives >= 1 && wordCheck > 0) {
+    		--lives;
+			info = document.getElementById('info');
+			info.innerHTML = "You did not guess, try again! " + lives + " lives left!";
+    	}
+    	if (lives == 0) {
+    	 	info = document.getElementById('info');
+			info.innerHTML = "I'm sorry, but you LOSE!";
+			for (i = 97; i <= 122; ++i) {
+				buttons[i].setAttribute("disabled", "");
+			}
+			var reset = document.createElement('button');
+			reset.innerHTML = "RESET";
+			document.getElementById('reset').appendChild(reset);
+	  		reset.onclick = function() {
+	  			for (i = 97; i <= 122; ++i) {
+	  				buttons[i].parentNode.removeChild(buttons[i]);
+	  			}
+	  			for (i = 0; i < underline.length; ++i) {
+	  				underline[i].parentNode.removeChild(underline[i]);
+	  			}
+	  			reset.parentNode.removeChild(reset);
+	  			play();
+	  		}
+    	}
+    	else if (lives >= 1 && wordCheck == 0) {
+    		info = document.getElementById('info');
+			info.innerHTML = "Congratulations! You WIN!";
+			var reset = document.createElement('button');
+			reset.innerHTML = "RESET";
+			document.getElementById('reset').appendChild(reset);
+	  		reset.onclick = function() {
+	  			for (i = 97; i <= 122; ++i) {
+	  				buttons[i].parentNode.removeChild(buttons[i]);
+	  			}
+	  			for (i = 0; i < underline.length; ++i) {
+	  				underline[i].parentNode.removeChild(underline[i]);
+	  			}
+	  			reset.parentNode.removeChild(reset);
+	  			play();
+	  		}
+    	}
+  	}
 }
+
+
 
